e84f0b5644e466e601e7b8f32f53add064fd9d0a
Harut Ter-Minasyan <hterminasyan@users.noreply.github.com>
2021-07-02 15:08:37 +0200

    Update external-catalog.js

--- srv/external-catalog.js
+++ srv/external-catalog.js
@@ -10,11 +10,7 @@ module.exports = cds.service.impl(async function () {
     this.on('READ', BusinessPartners, async (req) => {
         try {
             const bupaSrv = await cds.connect.to('OP_API_BUSINESS_PARTNER_SRV');
-            //let entity = 'A_BusinessPartner';
-            //let columnsToSelect = ["BusinessPartner", "FirstName", "LastName"];
-
             return await bupaSrv.tx(req).run(req.query)
-
         } catch (err) {
             req.reject(err);
         }
@@ -23,16 +19,8 @@ module.exports = cds.service.impl(async function () {
 
     this.on('READ', BusinessPartnerAddresses, async (req) => {
         try {
-
             const bupaSrv = await cds.connect.to('OP_API_BUSINESS_PARTNER_SRV');
-            //let entity = 'A_BusinessPartnerAddress';
-            //let columnsToSelect = ["BusinessPartner", "AddressID", "Country", "PostalCode", "CityName", "StreetName", "HouseNumber"];
             return await bupaSrv.tx(req).run(req.query)
-            /*return await bupaSrv.tx(req).run(
-                SELECT.from(entity)
-                    .columns(columnsToSelect)
-            )*/
-
         } catch (err) {
             req.reject(err);
         }
