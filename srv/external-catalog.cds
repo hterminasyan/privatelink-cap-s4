5d09933642a935e1053f765cdd2fd40673f8cdcb
Harutyun Ter-Minasyan <harutyun.ter-minasyan@sap.com>
2022-05-24 10:38:37 +0000

    cloud sdk trustStore

--- srv/external-catalog.cds
+++ srv/external-catalog.cds
@@ -2,10 +2,21 @@ using OP_API_BUSINESS_PARTNER_SRV as externalBuPa from './external/OP_API_BUSINE
 
 service S4Service {
     @cds.persistence.skip
-    entity BusinessPartners as projection on externalBuPa.A_BusinessPartner;
+    entity BusinessPartners         as
+        select from externalBuPa.A_BusinessPartner {
+            key BusinessPartner          as businessPartner,
+                BusinessPartnerFullName  as businessPartnerFullName,
+                BusinessPartnerIsBlocked as businessPartnerIsBlocked
+        };
 
     @cds.persistence.skip
     entity BusinessPartnerAddresses as projection on externalBuPa.A_BusinessPartnerAddress {
-        key BusinessPartner as businessPartnerId, AddressID as addressId, Country as country, CityName as cityName, StreetName as streetName, PostalCode as postalCode, HouseNumber as houseNumber
+        key BusinessPartner as BusinessPartner,
+            AddressID       as AddressID,
+            Country         as Country,
+            CityName        as CityName,
+            StreetName      as StreetName,
+            PostalCode      as PostalCode,
+            HouseNumber     as HouseNumber
     };
 }
--- srv/external-catalog.js
+++ srv/external-catalog.js
@@ -8,35 +8,23 @@ const { businessPartnerService } = require('@sap/cloud-sdk-op-vdm-business-partn
 //service handlers
 module.exports = cds.service.impl(async function () {
 
- 
+
     const { BusinessPartners, BusinessPartnerAddresses } = this.entities;
 
     this.on('READ', BusinessPartners, async (req) => {
         try {
 
-           /*const dest = await connectivity.getDestination({destinationName: "BusinessPartner"});
-            dest.url = dest.url + "/sap/opu/odata/sap/API_BUSINESS_PARTNER/A_BusinessPartner";
-            const responce = await httpClient.executeHttpRequest(dest, {method: "GET"})
-            console.log("huri:" + JSON.stringify(responce.data));
-
-            return [responce.data]*/
-
-            //const dest = await connectivity.getDestination({destinationName: "BusinessPartner"});
             const { businessPartnerApi } = businessPartnerService();
-            const anotherresult = await businessPartnerApi
-            .requestBuilder()
-            .getAll().select(
-                businessPartnerApi.schema.BUSINESS_PARTNER,
-                businessPartnerApi.schema.BUSINESS_PARTNER_FULL_NAME
-            ).top(100)
-            //.execute({destinationName: "BusinessPartner"});
-            .addCustomHeaders({ APIKey: '1GjgWZlnYc3bdTY0QzmKf8BLjFbSP9dp'})
-            .execute({
-                url: 'https://sandbox.api.sap.com/s4hanacloud'
-            });
-            console.log("huri:" + JSON.stringify(anotherresult));
-            return  anotherresult;
-        
+            const result = await businessPartnerApi
+                .requestBuilder()
+                .getAll().select(
+                    businessPartnerApi.schema.BUSINESS_PARTNER,
+                    businessPartnerApi.schema.BUSINESS_PARTNER_FULL_NAME,
+                    businessPartnerApi.schema.BUSINESS_PARTNER_IS_BLOCKED
+                ).top(100)
+                .execute({ destinationName: "BusinessPartner" });
+
+            return result;
 
         } catch (err) {
             req.reject(err);
@@ -46,8 +34,13 @@ module.exports = cds.service.impl(async function () {
 
     this.on('READ', BusinessPartnerAddresses, async (req) => {
         try {
-            const bupaSrv = await cds.connect.to('OP_API_BUSINESS_PARTNER_SRV');
-            return await bupaSrv.tx(req).run(req.query)
+
+            const dest = await connectivity.getDestination({ destinationName: "BusinessPartner" });
+            dest.url = dest.url + "/sap/opu/odata/sap/API_BUSINESS_PARTNER/A_BusinessPartnerAddress";
+            const responce = await httpClient.executeHttpRequest(dest, { method: "GET" })
+
+            return responce.data.d.results
+
         } catch (err) {
             req.reject(err);
         }
