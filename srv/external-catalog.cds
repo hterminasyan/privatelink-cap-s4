5b75053b776c896ad8d7a1e37d6e5cf0565e3be0
Harutyun Ter-Minasyan <harutyun.ter-minasyan@sap.com>
2022-05-24 09:20:52 +0000

    update

--- srv/external-catalog.cds
+++ srv/external-catalog.cds
@@ -2,13 +2,7 @@ using OP_API_BUSINESS_PARTNER_SRV as externalBuPa from './external/OP_API_BUSINE
 
 service S4Service {
     @cds.persistence.skip
-    entity BusinessPartners         as
-        select from externalBuPa.A_BusinessPartner {
-            key BusinessPartner          as businessPartnerId,
-                BusinessPartnerIsBlocked as businessPartnerIsBlocked,
-                FirstName                as businessPartnerFirstName,
-                LastName                 as businessPartnerLastName
-        };
+    entity BusinessPartners as projection on externalBuPa.A_BusinessPartner;
 
     @cds.persistence.skip
     entity BusinessPartnerAddresses as projection on externalBuPa.A_BusinessPartnerAddress {
--- srv/external-catalog.js
+++ srv/external-catalog.js
@@ -3,7 +3,7 @@
 const cds = require('@sap/cds-dk');
 const connectivity = require('@sap-cloud-sdk/connectivity');
 const httpClient = require('@sap-cloud-sdk/http-client')
-const { businessPartnerService } = require('@sap/cloud-sdk-vdm-business-partner-service');
+const { businessPartnerService } = require('@sap/cloud-sdk-op-vdm-business-partner-service');
 
 //service handlers
 module.exports = cds.service.impl(async function () {
@@ -13,28 +13,31 @@ module.exports = cds.service.impl(async function () {
 
     this.on('READ', BusinessPartners, async (req) => {
         try {
-            //const bupaSrv = await cds.connect.to('OP_API_BUSINESS_PARTNER_SRV');
-            //return await bupaSrv.tx(req).run(req.query)
-            //const r = await bupaSrv.tx(req).run(req.query)
-            const dest = await connectivity.getDestination({destinationName: "BusinessPartner"});
-            //dest.url = dest.url + "/sap/opu/odata/sap/API_BUSINESS_PARTNER/A_BusinessPartner";
-            //console.log(JSON.stringify(dest));
-            //const responce = await httpClient.executeHttpRequest(dest, {method: "GET"})
 
+           /*const dest = await connectivity.getDestination({destinationName: "BusinessPartner"});
+            dest.url = dest.url + "/sap/opu/odata/sap/API_BUSINESS_PARTNER/A_BusinessPartner";
+            const responce = await httpClient.executeHttpRequest(dest, {method: "GET"})
+            console.log("huri:" + JSON.stringify(responce.data));
 
+            return [responce.data]*/
+
+            //const dest = await connectivity.getDestination({destinationName: "BusinessPartner"});
             const { businessPartnerApi } = businessPartnerService();
             const anotherresult = await businessPartnerApi
             .requestBuilder()
-            .getAll()
-           /* .select(
-                businessPartnerApi.schema.FIRST_NAME,
-                businessPartnerApi.schema.LAST_NAME
-            )*/
-            .execute(dest);
-
-
+            .getAll().select(
+                businessPartnerApi.schema.BUSINESS_PARTNER,
+                businessPartnerApi.schema.BUSINESS_PARTNER_FULL_NAME
+            ).top(100)
+            //.execute({destinationName: "BusinessPartner"});
+            .addCustomHeaders({ APIKey: '1GjgWZlnYc3bdTY0QzmKf8BLjFbSP9dp'})
+            .execute({
+                url: 'https://sandbox.api.sap.com/s4hanacloud'
+            });
             console.log("huri:" + JSON.stringify(anotherresult));
             return  anotherresult;
+        
+
         } catch (err) {
             req.reject(err);
         }
